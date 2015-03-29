7880fcc51f6bd896822f2668dd675fe229a7cc1d
Jonathan Carter <jdcarter@uvm.edu>
2015-03-29 17:13:16 -0400

    MITH_Player update
    Not much of an update, but hopefully this commit will allow me to sync
    with what Joey did...

--- src/MITH_Player.java
+++ src/MITH_Player.java
@@ -1,5 +1,22 @@
+
+/*******************************************************************
+ * Moose In The House - CS205 Final Project
+ * Ian Benson, Jonathan Carter, Joey Palchak, Rachel Temple Cj Zhang
+ *
+ * MITH_Player - A base player class for Moose In The House
+ *******************************************************************/
+
+import java.awt.image.*;
+
 class MITH_Player {
 
 	private String name; // i am adding a comment
+	
+	private BufferedImage picture; // filename for a image for this player to be utilized by a GUI
+	
+	private MITH_Hand hand; // the player's hand
+	
+//	private String sound_filename; // filename root for the player's soundfile
+	
 
 }
