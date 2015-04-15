d1277bd50d455da1f0c9b04128189c2c95e03897
Jonathan Carter <jdcarter@uvm.edu>
2015-04-15 11:11:34 -0400

    30DPI images
    Added small images to use for the GUI

--- src/MITH_Player.java
+++ src/MITH_Player.java
@@ -7,6 +7,8 @@
  *******************************************************************/
 
 import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
 
 class MITH_Player {
 
@@ -16,7 +18,54 @@ class MITH_Player {
 	
 	private MITH_Hand hand; // the player's hand
 	
-//	private String sound_filename; // filename root for the player's soundfile
+	private MITH_House my_house; // the player's house cards
+	
+	private String soundFilestem; // filename root for the player's soundfile
+	
+	
+	/*********************************************************
+	 MITH_Player(name) - takes name and sets up picture based
+	 on the name of the player
+	 *********************************************************/
+	
+	public MITH_Player(String name, String pictureFilename, String soundFilestem) {
+		
+		hand = new MITH_Hand();
+		
+		this.name = name;
+		 
+		preparePicture(pictureFilename);
+		
+		this.soundFilestem = soundFilestem;
+			
+	}
 	
 
+	
+	/*********************************************************
+	 preparePicture - opens the image specified by filename
+	 and prepares the buffered image 
+	 *********************************************************/
+	private void preparePicture(String filename) {
+		String path = "..\\..\\resources\\images\\";
+		
+		try {
+			picture = ImageIO.read(new File(path + filename));
+		} catch (IOException e) {
+			System.out.println("Error opening player image: " + path + filename + ": " + e.getMessage());
+		}
+	}
+		
+		
+		
+	/*********************************************************
+	 main - testing and verification of the player class
+	 *********************************************************/
+	public static void main(String[] args) {
+		
+		MITH_Player player1 = new MITH_Player("Jonathan Carter", "jcarter.png", "jcarter");
+		
+	}
+		
+			
 }
